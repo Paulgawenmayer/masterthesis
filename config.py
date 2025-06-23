0670a6538b0d9934fe1804ae46f811f85bfcf6cf
Paulgawenmayer <paulgawenmayer@gmx.de>
2025-06-23 17:34:29 +0200

    API Key-Reset

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ Created on Tue Jun 10 14:43:23 2025
 
 @author: paulmayer
 
-This script provides the API-Key, nescssary for some scripts in this folder
+This file provides an API-Key, nescssary for some scripts in this folder. The Key below is an example and
+should be replaced with your own specific key.
 """
 
-API_KEY = "AIzaSyDeYPaDvN0XzEmLPAvUmHVCWZaTD6C8dM0"
+API_KEY = "AKlgkjwhegwegjgSLDWGZlhgsdzw"
